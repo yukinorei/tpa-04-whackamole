159e0b1427ba22042ea7d46b94fb7837acfea4df
yukinorei <>
2018-12-11 20:53:13 +0900

    checkPoint-1  コンポーネント化

---
+++ src/components/Counter.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class='counter'>
+        <h2>{{ label }}</h2>
+        <h1>{{ count }}</h1>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'Counter',
+  props: ['label', 'count'],
+};
+</script>
+
+<style>
+    .counter {
+    border: 1px solid #000;
+    margin-top: 20px;
+    padding: 20px;
+    }
+
+    .counter h1,
+    .counter h2 {
+    margin: 0;
+}
+</style>
