8795f923d08cfeb95c516942ececf97e22d88d95
yukinorei <>
2018-12-11 21:54:35 +0900

    修正コミット

--- src/components/Counter.vue
+++ src/components/Counter.vue
@@ -1,8 +1,8 @@
 <template>
-    <div class='counter'>
-        <h2>{{ label }}</h2>
-        <h1>{{ count }}</h1>
-    </div>
+  <div class='counter'>
+    <h2>{{ label }}</h2>
+    <h1>{{ count }}</h1>
+  </div>
 </template>
 
 <script>
@@ -13,14 +13,14 @@ export default {
 </script>
 
 <style>
-    .counter {
-    border: 1px solid #000;
-    margin-top: 20px;
-    padding: 20px;
-    }
+.counter {
+  border: 1px solid #000;
+  margin-top: 20px;
+  padding: 20px;
+}
 
-    .counter h1,
-    .counter h2 {
-    margin: 0;
+.counter h1,
+.counter h2 {
+  margin: 0;
 }
 </style>
--- src/components/Mole.vue
+++ src/components/Mole.vue
@@ -1,10 +1,10 @@
 <template>
-    <div class="mole-container inactive">
-        <div class="mole-image-container">
-          <img class="mole" src="../assets/mole.png" alt="mole"/>
-        </div>
-        <img class="dirt" src="../assets/dirt.svg" alt="mole dirt"/>
+  <div class="mole-container inactive">
+    <div class="mole-image-container">
+      <img class="mole" src="../assets/mole.png" alt="mole"/>
     </div>
+    <img class="dirt" src="../assets/dirt.svg" alt="mole dirt"/>
+  </div>
 </template>
 
 <script>
@@ -14,47 +14,46 @@ export default {
 </script>
 
 <style>
-    .mole-container {
-        width: 160px;
-        height: 160px;
-        display: inline-block;
-        margin: 10px;
-        position: relative;
-    }
+.mole-container {
+  width: 160px;
+  height: 160px;
+  display: inline-block;
+  margin: 10px;
+  position: relative;
+}
 
-    .mole-image-container {
-        overflow: hidden;
-        width: 160px;
-        height: 140px;
-    }
+.mole-image-container {
+  overflow: hidden;
+  width: 160px;
+  height: 140px;
+}
 
-    .mole-container img {
-        display: block;
-        transition: all 0.3s ease;
-    }
+.mole-container img {
+  display: block;
+  transition: all 0.3s ease;
+}
 
-    .mole {
-        position: relative;
-        width: 60%;
-        margin: auto;
-        cursor: pointer;
-    }
+.mole {
+  position: relative;
+  width: 60%;
+  margin: auto;
+  cursor: pointer;
+}
 
-    .mole-container.active .mole {
-        /* display: block; */
-        top: 0px;
-    }
+.mole-container.active .mole {
+  /* display: block; */
+  top: 0px;
+}
 
-    .mole-container.inactive .mole {
-        top: 200px;
-    }
+.mole-container.inactive .mole {
+  top: 200px;
+}
 
-    .dirt {
-        width: 100%;
-        margin: auto;
-        z-index: 1;
-        position: absolute;
-        bottom: 0;
-    }
-
+.dirt {
+  width: 100%;
+  margin: auto;
+  z-index: 1;
+  position: absolute;
+  bottom: 0;
+}
 </style>
